1dc7824a5cc9c7e3e5a2e015e9340331650a0916
Serj <sergius.volynets@gmail.com>
2019-04-13 18:45:34 +0300

    dragon move

--- scripts/image.js
+++ scripts/image.js
@@ -1,7 +1,24 @@
 var image = new Image();
+var timerId;
+function stInt (func) {
+     timerId = setInterval(func,100);
+}
 
 image.onload = function (){
-    ctx.drawImage(image,10,10,80,80);
+     stInt(move);
 };
 
 image.src = 'img/cuteDine.jpg';
+var x = 10;
+
+function move() {
+    if (x < 200) x +=5; else {clearInterval(timerId); stInt(moveBack);}
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(image,x,10,80,80);
+}
+
+function moveBack() {
+    if (x <= 200 && x>=10) x-=5; else {clearInterval(timerId);stInt(move);}
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(image,x,10,80,80);
+}
