9f42ac2cad27fa2dad8c5ffca49a4ceed2e98ce0
Serj <sergius.volynets@gmail.com>
2019-04-27 23:27:21 +0300

    4 chapter over come

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,8 +5,33 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
 
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == 'Right' || e.key == 'ArrowRight') {
+        rightPressed = true;
+    }
+    else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler (e) {
+    if (e.key == 'Right' || e.key == 'ArrowRight') {
+        rightPressed = false;
+    }
+    if (e.key == 'Left' || e.key == 'ArrowLeft') {
+        leftPressed = false;
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -14,19 +39,36 @@ function drawBall() {
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-};
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();    
+}
 
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
-    x += dx;
-    y += dy;
+    drawPaddle();
+  
     if(x+dx>canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if(y+dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
+    if(rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX +=7;
+    }
+    else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
 }
 
-setInterval(draw,20);
+setInterval(draw,10);
