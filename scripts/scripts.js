5de9fe8632f05a9a1fb606eea54e75731daf071a
Serj <sergius.volynets@gmail.com>
2019-04-29 19:14:47 +0300

    finished game

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,7 +31,7 @@ for (var i = 0; i<brickColumnCount; i++) {
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
+// document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if(e.key == 'Right' || e.key == 'ArrowRight') {
@@ -50,12 +50,12 @@ function keyUpHandler (e) {
         leftPressed = false;
     }
 }
-function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if(relativeX > 0 && relativeX < canvas.width) {
-        paddleX = relativeX - paddleWidth/2;
-    }
-}
+// function mouseMoveHandler(e) {
+//     var relativeX = e.clientX - canvas.offsetLeft;
+//     if(relativeX > 0 && relativeX < canvas.width) {
+//         paddleX = relativeX - paddleWidth/2;
+//     }
+// }
 
 function collisionDetection() {
     for(var i = 0; i<brickColumnCount; i++) {
